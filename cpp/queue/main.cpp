60ff5ff7adb3ec4e0120afcbd6526c33719540e0
Eray Mercan <eraymercan616@gmail.com>
2021-06-01 10:44:54 +0300

    c

--- cpp/queue/main.cpp
+++ cpp/queue/main.cpp
@@ -5,13 +5,23 @@
 
 int main(int argc, char const *argv[])
 {
+
+    
     char str[100], *u = (char *)malloc(sizeof(char));
     int i;
 
     QUEUE *sPr;
     sPr = createQueue();
+
     std::cout << "Bir cumle lutfen" << std::endl;
-    gets(str);
+
+    std::cin >> str;
+
+    for (int j = 0; j < 100; j++)
+    {
+        std::cout << str[j] << " , ";
+    }
+
     for (i = 0; str[i] != '\0'; i++)
     {
         if (str[i] != ' ')
@@ -19,11 +29,13 @@ int main(int argc, char const *argv[])
             enqueue(sPr, &str[i]);
         }
     }
+    //std::cout << (char) *(sPr->front->dataPtr) << std::endl;
     std::cout << "elemanlar yerlesti" << std::endl;
-    while(sPr->count != 0){
-        dequeue(sPr,(void **)&u);
-        std::cout<< *u <<std::endl;
-        u=(char*)malloc(sizeof(char));
+    while (sPr->count != 0)
+    {
+        dequeue(sPr, (void **)&u);
+        std::cout << *u << std::endl;
+        u = (char *)malloc(sizeof(char));
     }
 
     return 0;
