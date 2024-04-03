282a323221be248b4fc88a707616162c9511ecac
Eray Mercan <eraymercan616@gmail.com>
2024-04-03 09:22:57 +0300

    .

--- c/bignumbers/main.c
+++ c/bignumbers/main.c
@@ -6,9 +6,9 @@ int main(int argc, char const *argv[])
     struct Bignumber num1,num2;
     //initBignumber(&num1, "230102002009");
     initBignumberFromInt(&num1, 13435);
-    initBignumberFromInt(&num2, 13435);
+    initBignumberFromInt(&num2, 60000);
     printBignumberData(&num1);
-    subNumber(&num1,&num2);
+    addNumber(&num1,&num2);
     printBignumberData(&num1);
     printBignumberData(&num2);
 
